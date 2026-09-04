e02f280b7f9bf3cc9e73932a05e262ac7cab964d
Rajdeep Shaw <rajdeepshaw2006@gmail.com>
2026-09-05 01:31:59 +0530

    added  week 1 assignment problem solutions

---
+++ src/main/java/string/assigment_problems/TrafficSignalAnalyzer.java
@@ -0,0 +1,43 @@
+package string.assigment_problems;
+
+import java.util.Scanner;
+
+public class TrafficSignalAnalyzer {
+    public static void findLongestStreak(String signalLog) {
+        int currentStreak = 1;
+        int longestStreak = 1;
+        char longestColor = signalLog.charAt(0);
+        for (int i = 1; i < signalLog.length(); i++) {
+            if (signalLog.charAt(i) == signalLog.charAt(i - 1)) {
+                currentStreak++;
+            }
+            else {
+                currentStreak = 1;
+            }
+            if (currentStreak > longestStreak) {
+                longestStreak = currentStreak;
+                longestColor = signalLog.charAt(i);
+            }
+        }
+        System.out.println(
+            "Longest Streak: '"
+            + longestColor
+            + "' repeated "
+            + longestStreak
+            + " times"
+        );
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter signal log: ");
+        String signalLog = sc.nextLine();
+        if (signalLog.length() == 0) {
+            System.out.println("Signal log cannot be empty.");
+        }
+        else {
+            findLongestStreak(signalLog);
+        }
+        sc.close();
+    }
+}
